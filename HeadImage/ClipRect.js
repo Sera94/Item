49fa943df238ab0e65d4e51814d48952b1163af0
shenjiafei <2572790646@qq.com>
2016-10-17 18:30:32 +0800

    微博拍照修改

---
+++ HeadImage/ClipRect.js
@@ -0,0 +1,63 @@
+/**
+ * Created by Administrator on 2016/10/11 0011.
+ */
+
+//交互 抽象成一个对象
+
+window.hi = window.hi || {};
+
+(function () {
+
+    function ClipRect(x, y, width, heigth) {
+        this._width = width;
+        this._height = heigth;
+        this._x = x;
+        this._y = y;
+    }
+
+    var p = ClipRect.prototype;
+
+    p.draw = function (context) {
+        context.beginPath();
+        context.rect(this._x, this._y, this._width, this._height);
+        context.closePath();
+        context.clip();
+    };
+
+    Object.defineProperties(p, {
+        x: {
+            set: function (value) {
+                this._x = value;
+            },
+            get: function () {
+                return this._x;
+            }
+        },
+        y: {
+            set: function (value) {
+                this._y = value;
+            },
+            get: function () {
+                return this._y;
+            }
+        },
+        width: {
+            set: function (value) {
+                this._width = value;
+            },
+            get: function () {
+                return this._width;
+            }
+        },
+        height: {
+            set: function (value) {
+                this._height = value;
+            },
+            get: function () {
+                return this._height;
+            }
+        }
+    });
+
+    hi.ClipRect = ClipRect;
+})();
