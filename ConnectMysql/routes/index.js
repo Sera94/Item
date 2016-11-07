ec8c191ab29c48d084416bf9fb3b42b741d71c9b
shenjiafei <2572790646@qq.com>
2016-11-07 10:39:55 +0800

    1103修改

---
+++ ConnectMysql/routes/index.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+// const mysql = require('mysql');
+const createConn = require("../sources/MySqlConnection");
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+  // var conn = mysql.createConnection({
+  //   host: "localhost",
+  //   user: "root",
+  //   password: "",
+  //   database: "mydb"
+  // });
+
+  var conn = createConn();
+
+  conn.connect(function (err) {
+    if (!err) {
+      conn.query("SELECT * FROM `users`", function (err, rows) {
+        if (!err) {
+          res.render("index", {title: "用户操作", users: rows});
+        } else {
+          res.json(err);
+        }
+
+        conn.end();
+      });
+    } else {
+      res.json(err);
+    }
+  });
+});
+
+module.exports = router;
